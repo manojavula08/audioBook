047648daa7738a1841831ce3ff3ea8dfe5e765ae
“manojavula08” <manojreddy.avulamr@gmail.com>
2023-03-29 16:37:44 +0530

    backend_completed

--- main.py
+++ main.py
@@ -17,15 +17,3 @@ def pdf_audio():
     return RedirectResponse(url="/pdf-to-audio", status_code=status.HTTP_302_FOUND)
 
 models.Base.metadata.create_all(bind=engine)
-# with open("I20_Avula_Manoj.pdf", "rb") as file:
-#     reader = PyPDF2.PdfReader(file)
-#     text = reader.pages[0].extract_text()
-# engine1 = pyttsx3.init()
-# v = engine1.getProperty("voices")
-# engine1.setProperty("voice", v[0].id)
-# rate = engine1.getProperty("rate")
-# engine1.setProperty("rate", 175)
-# engine1.say(text)
-# engine1.say('My current speaking rate is ' + str(rate))
-# engine1.runAndWait()
-# engine1.stop()
